52cdfcfa6e0f74ddde61cd3dddee3e8e18c6b3fe
Caner Konuk <42453830+canerkonuk@users.noreply.github.com>
2023-10-27 02:14:38 +0300

    Solution created, class library project added.

---
+++ source/AzureStorageQueueAndTableCRUD/StorageAccountTableService.cs
@@ -0,0 +1,30 @@
+﻿using Azure.Data.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureStorageQueueAndTableCRUD
+{
+    public class StorageAccountTableService<T>: IStorageTableRepository<T> where T : ITableEntity
+    {
+
+        private readonly string _connectionString;
+        private readonly string _tableName;
+        private readonly string _partitionKey;
+        private TableClient _tableClient;
+
+        public StorageAccountTableService(string connectionString, string tableName, string partitionKey)
+        {
+            _connectionString = connectionString;
+            _tableName = tableName;
+            _tableClient = new TableClient(_connectionString, _tableName);
+            _tableClient.CreateIfNotExists();
+            _partitionKey = partitionKey;
+        }
+
+
+
+    }
+}
