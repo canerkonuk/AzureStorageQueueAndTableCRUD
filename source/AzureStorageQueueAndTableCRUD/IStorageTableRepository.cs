e389a12fa49084c45541a324ae28f3c6be34081e
Caner Konuk <42453830+canerkonuk@users.noreply.github.com>
2023-10-29 01:16:24 +0300

    Unit test project added to solution.

--- source/AzureStorageQueueAndTableCRUD/IStorageTableRepository.cs
+++ source/AzureStorageQueueAndTableCRUD/IStorageTableRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Azure.Data.Tables;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,9 @@ using System.Threading.Tasks;
 
 namespace AzureStorageQueueAndTableCRUD
 {
-    public interface IStorageTableRepository<T>
+    public interface IStorageTableRepository<T> where T : class, ITableEntity, new()
     {
-
+        string AddTableEntity(T entity);
+        Dictionary<string, string> GetEntitiesWithPartitionKey(string rowKey);
     }
 }
--- source/AzureStorageQueueAndTableCRUD/StorageAccountTableService.cs
+++ source/AzureStorageQueueAndTableCRUD/StorageAccountTableService.cs
@@ -1,4 +1,5 @@
-﻿using Azure.Data.Tables;
+﻿using Azure;
+using Azure.Data.Tables;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace AzureStorageQueueAndTableCRUD
 {
-    public class StorageAccountTableService<T>: IStorageTableRepository<T> where T : ITableEntity
+    public class StorageAccountTableService<T> : IStorageTableRepository<T> where T : class, ITableEntity, new()
     {
 
         private readonly string _connectionString;
@@ -24,6 +25,33 @@ namespace AzureStorageQueueAndTableCRUD
             _partitionKey = partitionKey;
         }
 
+        public string AddTableEntity(T entity)
+        {
+            var tableClientAddEntity = _tableClient.AddEntityAsync(entity);
+            tableClientAddEntity.Wait();
+            return tableClientAddEntity.Result.Status.ToString();
+        }
+
+
+        public Dictionary<string, string> GetEntitiesWithPartitionKey(string rowKey)
+        {
+            string partitionKeyFilter = $"PartitionKey eq '{_partitionKey}' and RowKey eq '{rowKey}'";
+            Pageable<T> pageableEntities = _tableClient.Query<T>(partitionKeyFilter);
+            var entity = pageableEntities.FirstOrDefault();
+
+            object entityValue;
+            Dictionary<string, string> entityKeyAndValues = new Dictionary<string, string>();
+            if (entity != null)
+            {
+                var entityKeys = entity;
+                //entityKeys.ForEach(entityKey =>
+                //{
+                //    entity.TryGetValue(entityKey, out entityValue);
+                //    entityKeyAndValues.Add(entityKey.ToString(), entityValue.ToString());
+                //});
+            }
+            return entityKeyAndValues;
+        }
 
 
     }
