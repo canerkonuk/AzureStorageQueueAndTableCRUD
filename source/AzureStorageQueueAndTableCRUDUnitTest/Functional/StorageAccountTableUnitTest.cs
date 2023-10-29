da275174fdbc3cf7bd92318aead55d47411f1de6
Caner Konuk <42453830+canerkonuk@users.noreply.github.com>
2023-10-30 00:54:42 +0300

    Added some storage table unit test functions.

--- source/AzureStorageQueueAndTableCRUDUnitTest/Functional/StorageAccountTableUnitTest.cs
+++ source/AzureStorageQueueAndTableCRUDUnitTest/Functional/StorageAccountTableUnitTest.cs
@@ -13,6 +13,58 @@ namespace AzureStorageQueueAndTableCRUDUnitTest.Functional
 {
     public class StorageAccountTableUnitTest
     {
-        
+        [Fact]
+        public void AddEntityIntoAzureStorageTableUnitTest()
+        {
+            // Arrange
+            var tableStorage = new StorageAccountTableService<TableEntity>("ConnectionString", "TestTableName", "TestPartitionKey");
+            var entitytest = new TableEntity("TestPartitionKey", "TestRowKey")
+            {
+                { "Property1", "Value1" },
+                { "Property2", "Value2" },
+                { "Property3", "Value3" }
+            };
+
+            // Act
+
+            var result = tableStorage.AddTableEntity(entitytest);
+
+            // Assert
+            result.Should().Be("204");
+        }
+
+
+        [Fact]
+        public void UpdateEntityIntoAzureStorageTableUnitTest()
+        {
+            // Arrange
+            var tableStorage = new StorageAccountTableService<TableEntity>("ConnectionString", "TestTableName", "TestPartitionKey");
+            var getEntityFromTable = tableStorage.GetEntitiesWithRowKeyFromPartition("TestRowKey");
+
+            //Adding new Key-Value's for updating entity:
+            getEntityFromTable["UpdateTest1"] = "UpdatedValue1";
+            getEntityFromTable["UpdateTest2"] = "UpdatedValue2";
+            getEntityFromTable["UpdateTest3"] = "UpdatedValue3";
+
+            // Act
+            var result = tableStorage.UpdateTableEntity(getEntityFromTable);
+
+            // Assert
+            result.Should().Be("204");
+        }
+
+
+        [Fact]
+        public void GetEntityFromAzureStorageTableUnitTest()
+        {
+            // Arrange
+            var tableStorage = new StorageAccountTableService<TableEntity>("ConnectionString", "TestTableName", "TestPartitionKey");
+
+            // Act
+            var result = tableStorage.GetEntitiesWithRowKeyFromPartition("TestRowKey");
+
+            // Assert
+            result.PartitionKey.Should().Be("TestPartitionKey");
+        }
     }
 }
